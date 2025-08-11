f60b383c26aef2cdcdadcf1ea5e4903819c4868d
nikhil <nikhiljavery59@gmail.com>
2025-08-11 22:08:06 +0530

    Chatv1

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+router.get("/",(req,res)=>{
+    res.render("index.ejs")
+})
+
+router.get("/chat",(req,res)=>{
+    res.render("chat.ejs");
+})
+
+
+module.exports  = router;
